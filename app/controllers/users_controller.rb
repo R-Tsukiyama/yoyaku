7448597a093b1b04c799e5fb61e3b15e973e74fc
tsukki <tsukki@tsukkinoMacBook-Air.local>
2023-06-26 20:18:04 +0900

    users-controller作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    if current_user.update(user_params)
+      falsh[:notice] = "更新しました"
+      redirect_to users_show_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :introduction,
+      :image
+    )
+  end
+end
